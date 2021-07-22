ddf3374422b6c48a0b52b1e91087d57d35d96354
Serdar45000 <serdar45000@gmail.com>
2021-07-22 06:16:34 +0200

    Homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,12 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        $posts = $postRepository->findBy([], ['id' => 'DESC'], 6);
+
+        return $this->render('home/index.html.twig', [
+            'posts' => $posts,
+        ]);
     }
 }
