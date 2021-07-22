04d11038b3d7ff2dae148bd917129f5c52a968d0
Serdar45000 <serdar45000@gmail.com>
2021-07-22 16:45:48 +0200

    Paginate

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,22 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\SearchPost;
+use App\Form\SearchPostType;
 use App\Repository\PostRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(PostRepository $postRepository): Response
+    public function index(Request $request, PostRepository $postRepository): Response
     {
         $posts = $postRepository->findBy([], ['id' => 'DESC'], 6);
+        $searchPost = new SearchPost();
+        $form = $this->createForm(SearchPostType::class, $searchPost);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $posts = $postRepository->findBySearch($searchPost);
+        }
 
         return $this->render('home/index.html.twig', [
-            'posts' => $posts,
+            'posts' => $posts ?? $postRepository->findAll(),
+            'form' => $form->createView(),
         ]);
     }
 }
